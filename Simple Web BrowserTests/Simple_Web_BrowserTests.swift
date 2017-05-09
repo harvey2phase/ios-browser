75cd8938092811c233f353f803d5687e09d3d910
Harvey Wang <harveywang@CN101046.local>
2017-05-09 15:58:18 -0300

    Initial Commit

---
+++ Simple Web BrowserTests/Simple_Web_BrowserTests.swift
@@ -0,0 +1,36 @@
+//
+//  Simple_Web_BrowserTests.swift
+//  Simple Web BrowserTests
+//
+//  Created by Harvey Wang on 2017-05-09.
+//  Copyright © 2017 Harvey Wang. All rights reserved.
+//
+
+import XCTest
+@testable import Simple_Web_Browser
+
+class Simple_Web_BrowserTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
